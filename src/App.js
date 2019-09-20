934e982ed3daa74d963c6921d8120bc610e6be81
Henry Doan <henrydoan96@gmail.com>
2019-09-19 20:42:30 -0600

    finished project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import List from './components/todos/List';
 import TodoForm from './components/todos/TodoForm';
+import Footer from './components/todos/Footer';
 
 class App extends Component {
   state = {
@@ -8,7 +9,13 @@ class App extends Component {
       { id: 1, name: 'Learn Rails', complete: true },
       { id: 2, name: 'Learn React', complete: false },
       { id: 3, name: 'Learn React Router', complete: false }
-    ]
+    ],
+    filter: 'All'
+  }
+
+  setFilter = (filter) => {
+                    // filter: filter
+    this.setState({ filter })
   }
 
   getUniqId = () => {
@@ -34,12 +41,40 @@ class App extends Component {
   //   })
   // }
 
-  render() {
+  handleClick = (id) => {
     const { todos } = this.state
+    this.setState({
+      todos: todos.map( todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            complete: !todo.complete
+          }
+        }
+        return todo
+      })
+    })
+  }
+
+  visibleItems = () => {
+    const { todos, filter } = this.state
+    switch(filter) {
+      case 'Active':
+        return todos.filter( t => !t.complete )
+      case 'Completed':
+        return todos.filter( t => t.complete )
+      default:
+        return todos
+    }
+  }
+
+  render() {
+    const { todos, filter } = this.state
     return (
       <div>
         <TodoForm addTodo={this.addTodo} />
-        <List name="Bucket List" items={todos} />
+        <List name="Bucket List" items={this.visibleItems()} todoClick={this.handleClick} />
+        <Footer filter={filter} setFilter={this.setFilter} />
       </div>
     )
   }
